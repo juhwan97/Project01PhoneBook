6cc7bf6bd3f96a4725c8fde77f8e746624b2e1b2
Juhwan Lee <93078305+juhwan97@users.noreply.github.com>
2021-10-28 13:26:00 +0900

    Add files via upload

---
+++ Project01Phonebook/src/project1/ver08/PhoneSchoolInfo.java
@@ -0,0 +1,16 @@
+package project1.ver08;
+
+public class PhoneSchoolInfo extends PhoneInfo {
+
+	String name;
+	String phoneNumber;
+	String major;//전공
+	int grade;//학년
+	
+	public PhoneSchoolInfo(String name, String phoneNumber,
+			String major, int grade) {
+		super(name, phoneNumber);
+		this.major = major;
+		this.grade = grade;
+	}
+}
