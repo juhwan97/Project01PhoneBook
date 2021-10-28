7289fa08f4446ffee56735fff3317e3814a1eb2c
Juhwan Lee <93078305+juhwan97@users.noreply.github.com>
2021-10-28 13:26:24 +0900

    Add files via upload

---
+++ Project01Phonebook/src/project1/PhoneBookVer08.java
@@ -0,0 +1,167 @@
+package project1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.ObjectInputStream;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import project1.ver08.AutoSaver;
+import project1.ver08.MenuSelectException;
+import project1.ver08.PhoneBookManager;
+import project1.ver08.PhoneInfo;
+import project1.ver08.SubMenuItem;
+
+//	public AutoSaveT(String n, String pn, String m, int g, String c) {
+//		this.name = n;
+//		this.phoneNumber = pn;
+//		this.major = m;
+//		this.grade = g;
+//		this.company = c;
+//		run();
+//	}
+//	public AutoSaveT(Object obj) {
+//		run();
+//	}
+//	PhoneInfo pi = new PhoneInfo(name, phoneNumber, major, grade, company);
+//	public AutoSaveT(String threadName) {
+//		super(threadName);
+//		run();
+//	}
+//	void threadMethod() throws InterruptedException, IOException {
+//		ObjectOutputStream out = new ObjectOutputStream(
+//			new FileOutputStream("src/project1/PhoneBook.obj"));
+//		out.writeObject(pi);
+//		sleep(5000);
+//	}	
+//	PhoneInfo pi = new PhoneInfo(name, phoneNumber, major, grade, company);
+//	@Override
+//	public void run() {
+//		try {
+//			threadMethod();
+//		}
+//		catch(InterruptedException e) {System.out.println("1오류");}
+//		catch(IOException e) {System.out.println("2오류");}
+//	}
+//	public void run() {
+//		try {
+//			ObjectOutputStream out = new ObjectOutputStream(
+//				new FileOutputStream("src/project1/PhoneBook.obj"));
+//			out.writeObject(pi);
+//			try {
+//				sleep(5000);
+//			}
+//			catch(InterruptedException e) {
+//				System.out.println("1오류발생");
+//				e.printStackTrace();
+//			}
+//		}
+//		catch(IOException e) {
+//			System.out.println("2오류발생");
+//			e.printStackTrace();
+//		}
+//	}
+//		while(true) {
+//			try {	
+//				ObjectOutputStream out = new ObjectOutputStream(
+//						new FileOutputStream("src/project1/PhoneBook.obj"));
+//					
+//				out.writeObject(new AutoSaveT());
+//				sleep(5000);
+//			}
+//			catch(InterruptedException e) {
+//				System.out.println("오류발생");
+//				e.printStackTrace();
+//			}
+//			catch(IOException e) {
+//				System.out.println("오류.무언가없음..");
+//				e.printStackTrace();
+//			}
+//		
+//	}
+
+public class PhoneBookVer08 {
+	
+	public static void printMenu() {
+		System.out.println("======================메뉴를 선택하세요====================");
+		System.out.println("1.주소록입력 2.검색 3.삭제 4.출력 5.저장옵션 6.종료 7.저장 데이터 확인");
+		System.out.println("===========================================================");
+		System.out.print("메뉴선택:"); 
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int select = 0;
+		PhoneBookManager pbm = new PhoneBookManager(100);
+		HashSet<Object> set = new HashSet<Object>();
+
+		while (select != 6) {
+			printMenu();
+			select = sc.nextInt();
+			
+			if(select>=1 && select<=7) {
+			try {
+				if(select==(SubMenuItem.input)) {
+					System.out.println("데이터 입력을 시작합니다.");
+					System.out.println("1.일반 2.동창 3.회사");
+					System.out.print("선택>>");
+					int select1_1 = sc.nextInt();
+					pbm.dataInput(select1_1);
+				}
+				else if(select==(SubMenuItem.search)) {
+					pbm.dataSearch();
+				}
+				else if(select==(SubMenuItem.delete)) {
+					pbm.dataDelete();
+				}
+				else if(select==(SubMenuItem.allShow)) {
+					pbm.dataAllShow();
+				}
+				else if(select==(SubMenuItem.save)) {
+					System.out.println("1.자동저장On\n2.자동저장Off\n선택:");
+					int saveselect = sc.nextInt();
+					if (saveselect==1) {
+						AutoSaver as = new AutoSaver(pbm);
+						as.start();
+						System.out.println("자동저장쓰레드 실행됨");
+					}
+					else if (saveselect==2) {
+						System.out.println("자동저장쓰레드 실행안됨");
+					}
+				}
+				else if(select==(SubMenuItem.exit)) {
+					System.out.println("프로그램종료");
+					break;
+				}
+				else if(select==7) {
+					pbm.scanInfoTxt();
+				}
+				}
+				catch(InputMismatchException e) {
+					sc.nextLine();
+					System.out.println("숫자를 입력하세요.");
+				}
+				catch(NullPointerException e) {
+					sc.nextLine();
+					System.out.println("검색결과가 없습니다.");
+				}		
+			}
+			else {
+				MenuSelectException mse = new MenuSelectException();
+				mse.getMessage();
+			}
+			
+		}
+
+	}
+}
+			
+
+	
+
